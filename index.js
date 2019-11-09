624487cbca18630598e034ac4d5ce352791dee20
nick.durbin <nick.durbin@gmail.com>
2019-11-09 17:23:20 -0500

    Built out the basic index for our server.

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 // implement your API here
-const http = require('http'
-)
+const http = require('http')
+
+const hostname ='127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain')
+  res.end('Hello World, from NodeJS')
+})
+
+server.listen(port, hostname, () => {
+  console.log(`server listening on http://${hostname}:${port}`)
+})
