c7398cc57b346c5696728c54fc9e88c2c0e69fc4
ndurbin <nick.durbin@gmail.com>
2019-11-09 22:37:02 -0500

    All endpoints are setup.

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ server.get('/', (req, res) => {
 
 server.get('/api/users', (req, res) => {
   database.find()
-    .then(users => {
-      res.status(200).json(users)
+    .then(data => {
+      res.status(200).json(data)
     })
     .catch(() => {
       return res.status(500).json({
@@ -25,9 +25,9 @@ server.get('/api/users', (req, res) => {
 
 server.get('/api/users/:id', (req, res) => {
   database.findById(req.params.id)
-    .then(user => {
-      return user 
-      ? res.status(200).json(user)
+    .then(data => {
+      return data 
+      ? res.status(200).json(data)
       : res.status(404).json({
         message: "The user with the specified ID does not exist."
       })
@@ -42,8 +42,8 @@ server.get('/api/users/:id', (req, res) => {
 // DELTE REQUESTS
 server.delete('/api/users/:id', (req, res) => {
   database.remove(req.params.id)
-    .then(user => {
-      return user !== 0
+    .then(data => {
+      return data !== 0
         ? res.status(200).json({ message: "Success!" })
         : res.status(404).json({
           message: "The user with the specified ID does not exist."
@@ -58,34 +58,44 @@ server.delete('/api/users/:id', (req, res) => {
 
 // UPDATE REQUESTS
 server.put('/api/users/:id', (req, res) => {
-  database.findById(req.params.id)
-    .then(user => {
-      return user
-      ? res.status(200).json(user)
+  if (!req.body.name || req.body.bio) {
+    return res.status(400).json({
+      error: "Okease provide name and bio for the user."
+    })
+  }
+  database.update(req.body)
+    .then(data => {
+      return data !== 0
+      ? res.status(200).json({ ...data, ...req.body })
       : res.status(404).json({
         message: "The user with the specified ID does not exist."
       })
     })
     .catch(() => {
       return res.status(500).json({
-        error: "You had one job to do and failed."
+        error: "The user information could not be modified."
       })
     })
 })
 
 //CREATE REQUESTS
 server.get('/api/users', (req, res) => {
-  database.find()
-    .then(users => {
-      res.status(200).json(users)
+  if (!req.body.name || !req.body.bio) {
+    return res.status(400).json({
+      error: "Please provide name and bio of the user."
+    })
+  }
+  database.insert(req.body)
+    .then(data => {
+      res.status(200).json({ ...data, ...req.body})
     })
     .catch(() => {
       return res.status(500).json({
-        error: "You had one job to do and failed."
+        error: "There was an error while saving the user to the database."
       })
     })
 })
 
-server.listen(5000, () => {
-  console.log('Server running on http://localhost:5000')
+server.listen(process.env.PORT || 5000, () => {
+  console.log("Server listening on" + (process.env.PORT || 5000))
 })
