7900ace3a88c9fba60b8d4ea197f0604a67365d2
ndurbin <nick.durbin@gmail.com>
2019-11-09 22:24:57 -0500

    Have most of the api end points set up.

--- index.js
+++ index.js
@@ -6,8 +6,84 @@ const server = express()
 server.use(express.json())
 server.use(cors())
 
+// GET REQUESTS
 server.get('/', (req, res) => {
   res.send('Hello from Express')
+})
+
+server.get('/api/users', (req, res) => {
+  database.find()
+    .then(users => {
+      res.status(200).json(users)
+    })
+    .catch(() => {
+      return res.status(500).json({
+        error: "The users information could not be retrieved."
+      })
+    })
+})
+
+server.get('/api/users/:id', (req, res) => {
+  database.findById(req.params.id)
+    .then(user => {
+      return user 
+      ? res.status(200).json(user)
+      : res.status(404).json({
+        message: "The user with the specified ID does not exist."
+      })
+    })
+    .catch(() => {
+      return res.status(500).json({
+        error: "The user information could not be retrieved."
+      })
+    })
+})
+
+// DELTE REQUESTS
+server.delete('/api/users/:id', (req, res) => {
+  database.remove(req.params.id)
+    .then(user => {
+      return user !== 0
+        ? res.status(200).json({ message: "Success!" })
+        : res.status(404).json({
+          message: "The user with the specified ID does not exist."
+        })
+      })
+    .catch(() => {
+      return res.status(500).json({
+        error: "The user could not be removed."
+      })
+    })
+})
+
+// UPDATE REQUESTS
+server.put('/api/users/:id', (req, res) => {
+  database.findById(req.params.id)
+    .then(user => {
+      return user
+      ? res.status(200).json(user)
+      : res.status(404).json({
+        message: "The user with the specified ID does not exist."
+      })
+    })
+    .catch(() => {
+      return res.status(500).json({
+        error: "You had one job to do and failed."
+      })
+    })
+})
+
+//CREATE REQUESTS
+server.get('/api/users', (req, res) => {
+  database.find()
+    .then(users => {
+      res.status(200).json(users)
+    })
+    .catch(() => {
+      return res.status(500).json({
+        error: "You had one job to do and failed."
+      })
+    })
 })
 
 server.listen(5000, () => {
