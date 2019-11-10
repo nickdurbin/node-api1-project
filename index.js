a89fbe30baa4dedfc7f89bb16a64e933e29d9128
ndurbin <nick.durbin@gmail.com>
2019-11-09 20:51:55 -0500

    Reconfigured to us express boilerplate with cors

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-// implement your API here
-const http = require('http'
-)
+const express = require('express')
+const cors = require('cors')
+const database = require('./data/db')
+const server = express()
+
+server.use(express.json())
+server.use(cors())
+
+server.get('/', (req, res) => {
+  res.send('Hello from Express')
+})
+
+server.listen(5000, () => {
+  console.log('Server running on http://localhost:5000')
+})
