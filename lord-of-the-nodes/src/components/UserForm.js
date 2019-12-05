6ed0d33d03719d9a25c48c5ce99e5890f42df90b
ndurbin <nick.durbin@gmail.com>
2019-12-05 03:17:09 -0500

    Moved in a userlist and changed logic.

--- lord-of-the-nodes/src/components/UserForm.js
+++ lord-of-the-nodes/src/components/UserForm.js
@@ -1,38 +1,70 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import UserList from './UserList';
 import axios from 'axios';
 
-function UserForm(props) {
-  const [users, setUsers] = useState([])
-  console.log(users)
+function UserForm({ users, setUsers }) {
+  const [editing, setEditing] = useState(false)
+  const [userToEdit, setUserToEdit] = useState({
+    id: '',
+    name: '',
+    bio: ''
+  })
   const [formValues, setFormValues] = useState({
     name: '',
     bio: ''
   })
 
+  const editUser = user => {
+    setEditing(true);
+    setUserToEdit(user);
+  };
 
-
-  useEffect(() => {
-    axios
-      .get('http://127.0.0.1:8080/api/users')
-      .then(res => {
-        setUsers(res.data)
-      })
-      .catch(err => {
-        console.log(err.response, 'You did not get the users.')
-      })
-  }, [])
-
-  const handleChange = (e) => {
+  const handleChange = e => {
     setFormValues({
+      ...formValues,
       [e.target.name]: e.target.value
     })
   }
 
-  const handleSubmit = (e) => {
+  const handleSubmit = e => {
     e.preventDefault()
-    setUsers(formValues)
+    axios
+    .post('http://127.0.0.1:8080/api/users', formValues)
+    .then(res => {
+      console.log(res.data, 'User successfully created!')
+      return setUsers(res.data)
+    })
+    .catch(err => {
+      console.log(err.response, 'User not created.')
+    })
     resetForm()
   }
+
+  const deleteUser = id => {
+    if (window.confirm('Are you sure you want to delete user?'))
+    axios
+      .delete(`http://127.0.0.1:8080/api/users/${id}`)
+      .then(res => {
+        console.log(res.data, 'User successfully deleted.')
+        return setUsers(res.data)
+      })
+      .catch(err => {
+        console.log(err.response, 'User still here.')
+      })
+  }
+
+  const saveEdit = e => {
+    e.preventDefault()
+    axios
+      .put(`http://127.0.0.1:8080/api/users/${userToEdit.id}`, userToEdit)
+			.then(result => {
+        setEditing(false)
+        console.log('User was edited!')
+			})
+			.catch(error => {
+				console.log(error)
+			})
+	}
 
   const resetForm = () => {
     setFormValues({
@@ -42,25 +74,40 @@ function UserForm(props) {
   }
 
   return (
-    <>
-    <form onSubmit={handleSubmit}>
-      <h1>User Form</h1>
+    <div>
+     {editing && (
+        <form onSubmit={saveEdit}>
+            <h1>Update User</h1>
+            <input 
+              onChange={e => setUserToEdit({ userToEdit, name: e.target.value })}
+              value={userToEdit.name}
+              required
+            />
 
-      <input type="text" name="name" placeholder="Name" required onChange={handleChange} />
-
-      <input type="text" name="bio" placeholder="Bio" required onChange={handleChange} />
-
-      <button className="formButton" type="submit">Submit!</button>
-    </form>
-    {users.map((user, index) => {
-        return (
-          <div className='userContainer' key={index} index={index}>
-            <h1>Character</h1>
-            <h2>Name: {user.name}</h2>
-            <h4>Bio: {user.bio}</h4>
+            <input
+              onChange={e =>  setUserToEdit({ userToEdit, bio: e.target.value })}
+              value={userToEdit.bio}
+              required
+            />
+          <div className="btnContainer">
+            <button type="submit">Save</button>
+            <button onClick={() => setEditing(false)}>Cancel</button>
           </div>
-      ) })}
-     </>
+        </form>
+      )}
+    {!editing && (
+      <form onSubmit={handleSubmit}>
+        <h1>User Form</h1>
+
+        <input type="text" name="name" placeholder="Name" value={formValues.name} required onChange={handleChange} />
+
+        <input type="text" name="bio" placeholder="Bio" value={formValues.bio} required onChange={handleChange} />
+
+        <button className="formButton" type="submit">Submit!</button>
+      </form>
+    )}
+      <UserList editUser={editUser} deleteUser={deleteUser} users={users}/>
+    </div>
   );
 }
 
