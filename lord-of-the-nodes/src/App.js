0c731938d68cab2716d10d5d50b4ccc4d0aed045
ndurbin <nick.durbin@gmail.com>
2019-12-05 01:35:19 -0500

    Set up the app to display the user info form.

---
+++ lord-of-the-nodes/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import UserForm from './components/UserForm';
+
+function App() {
+
+  return (
+    <div className="mainContent">
+      <UserForm />
+    </div>
+  );
+}
+
+export default App;
