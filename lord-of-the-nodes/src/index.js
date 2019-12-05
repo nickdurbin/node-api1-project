f19af87542577f2d162ad77a99d88c146d227f6e
ndurbin <nick.durbin@gmail.com>
2019-12-05 04:30:40 -0500

    Setup the Redux store.

--- lord-of-the-nodes/src/index.js
+++ lord-of-the-nodes/src/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { Provider } from 'react-redux';
+import { userReducer as reducer } from './reducers/userReducer';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
+const store = createStore(reducer, applyMiddleware(thunk, logger))
+
+ReactDOM.render(
+<Provider store={store}>
+  <Router>
+    <App />
+  </Router>
+</Provider>,
+document.getElementById('root'));
