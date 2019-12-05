76dd0849d7b8ed2cdebd1228b0aa40b31f369a14
ndurbin <nick.durbin@gmail.com>
2019-12-05 04:30:24 -0500

    Setup Redux and the store.

--- lord-of-the-nodes/src/App.js
+++ lord-of-the-nodes/src/App.js
@@ -1,30 +1,19 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import './App.css';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { getUsers } from './actions/userActions';
 import UserForm from './components/UserForm';
-import UserList from './components/UserList';
-import { Switch, Route, withRouter } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 
 function App() {
-  const [users, setUsers] = useState([])
+  const dispatch = useDispatch()
 
   useEffect(() => {
-    axios
-      .get('http://127.0.0.1:8080/api/users')
-      .then(res => {
-        setUsers(res.data)
-      })
-      .catch(err => {
-        console.log(err.response, 'You did not get the users.')
-      })
-  }, [users])
+    dispatch(getUsers())
+  }, [dispatch])
 
   return (
     <div className="mainContent">
-      <Switch>
-        <Route exact path='/' render={props => <UserForm {...props} users={users} />} />
-        <Route path='/users' render={props => <UserList {...props} users={users} />} />
-      </Switch>
+      <UserForm />
     </div>
   );
 }
